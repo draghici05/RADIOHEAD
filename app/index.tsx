a2daca126f1460684a7798b3858960802912656e
draghici05 <alexdraghici190@gmail.com>
2025-03-09 16:25:52 +0200

    ?

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Link } from 'expo-router'
+
+const index = () => {
+  return (
+    <View style={styles.container}>
+      <Text>index</Text>
+      <Link href="/input" style={{ color: 'blue' }}>Go to input</Link>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
